1ad7d85d206d9df940bce86f6005acdc291a2398
alecmcclendon <malec7858@gmail.com>
2025-05-11 17:12:13 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,2 @@
-#!/usr/bin/env python3
 import http.server
-import socketserver
-import os
-
-PORT = 8000
-
-# Set the working directory for the server
-os.chdir('/opt/render/project/src')
-
-# Configure CGI handler
-Handler = http.server.CGIHTTPRequestHandler
-Handler.cgi_directories = ["/cgi-bin"]
-
-print(f"Serving at port {PORT}")
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    httpd.serve_forever()
+http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler)
