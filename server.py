e1c0f6ad0d6bd60501dcba79c240bc67c32894f1
alecmcclendon <malec7858@gmail.com>
2025-05-11 16:55:02 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,30 +1,14 @@
-from flask import Flask, send_file
-import os
-import random
+#!/usr/bin/env python3
+# server.py
+import http.server
+import socketserver
+import cgi
 
-app = Flask(__name__)
+PORT = 8000
 
-# Serve the homepage
-@app.route('/')
-def index():
-    return send_file('home.html')
+Handler = http.server.CGIHTTPRequestHandler
+Handler.cgi_directories = ["/cgi-bin"]
 
-# Serve the summon route
-@app.route('/summon')
-def summon():
-    videos = os.listdir('vid')
-    chosen_video = random.choice(videos)
-    print(f"Serving video: {chosen_video}")
-    return send_file(f'vid/{chosen_video}')
-
-# Serve the QR route
-@app.route('/qr')
-def qr():
-    qr_images = os.listdir('qr')
-    chosen_qr = random.choice(qr_images)
-    print(f"Serving QR image: {chosen_qr}")
-    return send_file(f'qr/{chosen_qr}')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
