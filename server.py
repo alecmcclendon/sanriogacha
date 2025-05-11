71acf37c1e57f3fc3e0151d3678a7e24df64ffcf
alecmcclendon <malec7858@gmail.com>
2025-05-11 17:36:33 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,2 +1,5 @@
-import http.server
-http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler)
+import os
+from http.server import test, CGIHTTPRequestHandler
+
+port = int(os.environ.get("PORT", 8000))
+test(HandlerClass=CGIHTTPRequestHandler, port=port)
