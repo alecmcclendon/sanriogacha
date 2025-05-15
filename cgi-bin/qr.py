db760f4a29a1751a8ce69052dd38cbda016edbf6
alecmcclendon <malec7858@gmail.com>
2025-05-16 01:40:16 +0900

    Update qr.py

--- cgi-bin/qr.py
+++ cgi-bin/qr.py
@@ -11,7 +11,7 @@ HTML_Head = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>シナモロール</title>
-    <link rel="stylesheet" href="../css/qr.css">
+    <link rel="stylesheet" href="../css/qr.css?v=2">
 </head>
 """
  
